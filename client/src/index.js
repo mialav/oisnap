239490afc31290dbf739de0065da77a989839ce8
Mia Lavanti <mialavanti@Mias-MacBook-Air-3.local>
2020-02-27 15:03:18 +0100

    added map component in client src, added reference to mapbox in index.html and added mapbox style to App.css

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+mapboxgl.accessToken = "MAPBOX_ACCESS_TOKEN";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
