7d9aeca34b1b342f6b396947e1560c81125c75d7
Nora Nicklis <nora.nicklis@gmail.com>
2020-02-27 10:43:29 +0100

    message in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Nora Branch!
         </p>
+        <p>This is the best branch ever!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
