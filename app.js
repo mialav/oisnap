9482103fe9cc9afe57214faebe58d4dbe016769d
Nora Nicklis <nora.nicklis@gmail.com>
2020-03-03 12:05:17 +0100

    fix URI MONGO

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost/backend", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/backend", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
